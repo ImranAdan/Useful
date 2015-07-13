5f4f69e383c6dad8c32d721386c9d9d0e5758fc2
Imran Adan <imranadan@gmail.com>
2015-07-13 21:33:53 +0100

    serialisation example in C#

---
+++ c#/serialisation/MyPojo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulC_SHARP {
+    public class MyPojo {
+        
+        private string name;
+        private byte[] content;
+
+        public string Name {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
---
+++ c#/serialisation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace UsefulC_SHARP {
+    public class Program {
+        public static void Main(string[] args) {
+
+            MyPojo b = new MyPojo();
+            b.Name = "Hello World";
+
+            XmlSerializer s = new XmlSerializer(b.GetType());
+
+            StringWriter sww = new StringWriter();
+            XmlWriter writer = XmlWriter.Create(sww);
+            s.Serialize(writer, b);
+
+            var xml = sww.ToString();
+            Console.WriteLine(xml);
+            
+            Console.ReadLine();
+        }
+    }
+}
