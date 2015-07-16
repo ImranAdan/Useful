474d7b9472a56053110a50d84ba22d88bf79e374
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-07-16 13:21:10 +0100

    Fixed Dijkstra implementation and tests

--- java/dijkstra/test/DijkstraTest.java
+++ java/dijkstra/test/DijkstraTest.java
@@ -29,79 +29,79 @@ public class DijkstraTest {
     }
 
     @Test
-    public void TestExplicitRoute_ABC_Expect9() throws SatNav.NoSuchRouteException {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(9, satNav.getDistanceOfExplicitRoute("A-B-C"));
+    public void TestExplicitRoute_ABC_Expect9() throws Dijkstra.NoSuchRouteException {
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(9, dijkstra.getDistanceOfExplicitRoute("A-B-C"));
     }
 
     @Test
-    public void TestExplicitRoute_AD_Expect5() throws SatNav.NoSuchRouteException {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(5, satNav.getDistanceOfExplicitRoute("A-D"));
+    public void TestExplicitRoute_AD_Expect5() throws Dijkstra.NoSuchRouteException {
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(5, dijkstra.getDistanceOfExplicitRoute("A-D"));
     }
 
     @Test
-    public void TestExplicitRoute_ADC_Expect13() throws SatNav.NoSuchRouteException {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(13, satNav.getDistanceOfExplicitRoute("A-D-C"));
+    public void TestExplicitRoute_ADC_Expect13() throws Dijkstra.NoSuchRouteException {
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(13, dijkstra.getDistanceOfExplicitRoute("A-D-C"));
     }
 
     @Test
-    public void TestExplicitRoute_AEBCD_Expect21() throws SatNav.NoSuchRouteException {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(21, satNav.getDistanceOfExplicitRoute("A-E-B-C-D"));
+    public void TestExplicitRoute_AEBCD_Expect21() throws Dijkstra.NoSuchRouteException {
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(21, dijkstra.getDistanceOfExplicitRoute("A-E-B-C-D"));
     }
 
-    @Test(expected = SatNav.NoSuchRouteException.class)
-    public void TestExplicitRoute_AED_ExpectNoSuchRouteException() throws SatNav.NoSuchRouteException {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(0, satNav.getDistanceOfExplicitRoute("A-E-D"));
+    @Test(expected = Dijkstra.NoSuchRouteException.class)
+    public void TestExplicitRoute_AED_ExpectNoSuchRouteException() throws Dijkstra.NoSuchRouteException {
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(0, dijkstra.getDistanceOfExplicitRoute("A-E-D"));
     }
 
     @Test
     public void TestRoutesWithMax3Junctions_CC_Expect2() {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(2, satNav.routesWithMaxJunctions("C", "C", 3));
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(2, dijkstra.routesWithMaxJunctions("C", "C", 3));
         //Check the routes found have all the expected routes
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-D-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-D-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-E-B-C"));
     }
 
     @Test
     public void TestRoutesWith4Junctions_AC_Expect3() {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(3, satNav.routesWithJunctions("A", "C", 4));
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(3, dijkstra.routesWithJunctions("A", "C", 4));
         //Check the routes found have all the expected routes
-        assertTrue(satNav.getPrintedFoundRoutes().contains("A-B-C-D-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("A-D-C-D-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("A-D-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("A-B-C-D-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("A-D-C-D-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("A-D-E-B-C"));
     }
 
     @Test
     public void TestShortestRoute_AC_Expect9() {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(9, satNav.shortestRoute("A", "C"));
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(9, dijkstra.shortestRoute("A", "C"));
     }
 
     @Test
     public void TestShortestRoute_BB_Expect9() {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(9, satNav.shortestRoute("B", "B"));
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(9, dijkstra.shortestRoute("B", "B"));
     }
 
     @Test
     public void TestRoutesDistanceLessThan30_CC_Expect9() {
-        SatNav satNav = new SatNav(listOfRoads);
-        assertEquals(9, satNav.routesWithDistanceLessThan("C", "C", 30));
+        Dijkstra dijkstra = new Dijkstra(listOfRoads);
+        assertEquals(9, dijkstra.routesWithDistanceLessThan("C", "C", 30));
         //Check the routes found have all the expected routes
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-D-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-E-B-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-E-B-C-D-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-D-C-E-B-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-D-E-B-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-E-B-C-E-B-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-E-B-C-E-B-C-E-B-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-D-E-B-C-E-B-C"));
-        assertTrue(satNav.getPrintedFoundRoutes().contains("C-E-B-C-D-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-D-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-E-B-C-D-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-D-C-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-D-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-E-B-C-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-E-B-C-E-B-C-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-D-E-B-C-E-B-C"));
+        assertTrue(dijkstra.getPrintedFoundRoutes().contains("C-E-B-C-D-E-B-C"));
     }
 }
